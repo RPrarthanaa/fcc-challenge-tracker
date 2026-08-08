8711a77f63f31c60d4810aaec18df22d12a8b4e0
Prarthanaa <prarthanaa07@gmail.com>
2026-08-08 10:32:19 +0530

    Set up the server environment and initial routing

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,14 +7,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Backend is running"
-    });
-});
+const challengeRoutes = require("./routes/challengeRoutes");
+app.use("/api", challengeRoutes);
+
+const calendarRoutes = require("./routes/calendarRoutes");
+app.use("/api", calendarRoutes);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
