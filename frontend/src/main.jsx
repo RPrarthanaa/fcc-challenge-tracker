b6f44242af33ed75f2b0b65fa52da2e5d96ea88a
Prarthanaa <prarthanaa07@gmail.com>
2026-08-29 10:41:07 +0530

    Configured React Router to use the GitHub Pages repository path

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/fcc-challenge-tracker">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
